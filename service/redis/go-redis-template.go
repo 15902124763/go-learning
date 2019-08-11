d95e7f46e39f756a7844904f3f280e05c8807582
yarm <15902124763@163.com>
2019-08-11 09:09:31 +0800

    #golang echo

--- service/redis/go-redis-template.go
+++ service/redis/go-redis-template.go
@@ -2,25 +2,28 @@ package redis
 
 import (
 	"github.com/go-redis/redis"
-	"go-learning/log"
 	"go-learning/conf"
+	"go-learning/log"
 )
 
 var server = conf.Conf.Redis.Server
 var port = conf.Conf.Redis.Port
 var password = conf.Conf.Redis.Password
 var db = conf.Conf.Redis.DB
-var addr  = server + ":" + string(port)
-
+var addr = server + ":" + string(port)
 
 // 获取连接
-func GetClient()  {
+func GetClient() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: password, // no password set
-		DB:       db,  // use default DB
+		DB:       db,       // use default DB
 	})
 
 	pong, err := client.Ping().Result()
 	log.Infof(pong, err)
+}
+
+func set() {
+
 }
